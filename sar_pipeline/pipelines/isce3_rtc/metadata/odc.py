54db85ed1c61eda14a76b616dd8729b28066b854
abradley60 <alexbradley60@gmail.com>
2026-03-12 01:14:12 +0000

    fix browse url for different buckets

--- sar_pipeline/pipelines/isce3_rtc/metadata/odc.py
+++ sar_pipeline/pipelines/isce3_rtc/metadata/odc.py
@@ -172,6 +172,7 @@ def make_rtc_s1_static_product_browse_url(
     s3_bucket: str,
     rtc_s1_static_s3_prefix: str,
     s3_region: str = "ap-southeast-2",
+    base_url = None
 ) -> str:
     """Make the browse url to the static layer product from the paths provided
 
@@ -183,6 +184,9 @@ def make_rtc_s1_static_product_browse_url(
         product folder s3 path
     s3_region : str, optional
         aws region code, by default "ap-southeast-2"
+    base_url :
+        The base url before the product s3_prefix. If not provided, assume
+        hardcoded option based on s3_bucket name and existing settings.
 
     Returns
     -------
@@ -190,17 +194,27 @@ def make_rtc_s1_static_product_browse_url(
         The url to the index file where static layers are stored for user
         visibility
     """
+    # hardcode by bucket if an alternative base url not provided
+    if not base_url:
+        if s3_bucket == "deant-data-public-dev":
+            url = f"https://{s3_bucket}.s3.{s3_region}.amazonaws.com/index.html?prefix={rtc_s1_static_s3_prefix}"
+        elif s3_bucket == "dea-public-data-dev" :
+            url = f'https://data.dev.dea.ga.gov.au/?prefix={rtc_s1_static_s3_prefix}'
+        elif s3_bucket == "dea-public-data":
+            url = f'https://data.dea.ga.gov.au/?prefix={rtc_s1_static_s3_prefix}'
+        else:
+            raise ValueError('Unknown browse URL for s3 bucket, please provide a `base_url`.')
+    else:
+        url = f'{base_url}{rtc_s1_static_s3_prefix}'
 
-    return (
-        f"https://{s3_bucket}.s3.{s3_region}.amazonaws.com"
-        f"/index.html?prefix={rtc_s1_static_s3_prefix}"
-    )
+    return url
 
 
 def make_rtc_s1_product_browse_url(
     s3_bucket: str,
     rtc_s1_s3_prefix: str,
     s3_region: str = "ap-southeast-2",
+    base_url = None
 ):
     """Make the browse url for the burst product.
 
@@ -212,6 +226,9 @@ def make_rtc_s1_product_browse_url(
         s3 subfolder for product
     s3_region : str, optional
         aws region code, by default "ap-southeast-2"
+    base_url :
+        The base url before the product s3_prefix. If not provided, assume
+        hardcoded option based on s3_bucket name and existing settings.
 
     Returns
     -------
@@ -220,21 +237,34 @@ def make_rtc_s1_product_browse_url(
         visibility
     """
 
-    return (
-        f"https://{s3_bucket}.s3.{s3_region}.amazonaws.com"
-        f"/index.html?prefix={rtc_s1_s3_prefix}"
-    )
+     # hardcode by bucket if an alternative base url not provided
+    if not base_url:
+        if s3_bucket == "deant-data-public-dev":
+            url = f"https://{s3_bucket}.s3.{s3_region}.amazonaws.com/index.html?prefix={rtc_s1_s3_prefix}"
+        elif s3_bucket == "dea-public-data-dev" :
+            url = f'https://data.dev.dea.ga.gov.au/?prefix={rtc_s1_s3_prefix}'
+        elif s3_bucket == "dea-public-data":
+            url = f'https://data.dea.ga.gov.au/?prefix={rtc_s1_s3_prefix}'
+        else:
+            raise ValueError('Unknown browse URL for s3 bucket, please provide a `base_url`.')
+    else:
+        url = f'{base_url}{rtc_s1_s3_prefix}'
+
+    return url
 
 
-def get_s3_prefix_from_product_browse_url(browse_url: str) -> str:
-    """Get the s3_subpath from the browse link. Removes the path following "?prefix="
+def get_s3_prefix_from_product_browse_url(browse_url: str, base_url=None) -> str:
+    """Get the s3_subpath from the browse link. Removes the path following "?prefix=",
+    unless a base_url is provided.
 
     Parameters
     ----------
     browse_url : str
         A URL to browse either the rtc_s1 or rtc_s1_static product subfolder created
         with either make_static_layer_browse_url or make_rtc_s1_burst_browse_url.
-
+    base_url :
+        The base url before the product s3_prefix. If not provided, assume
+        hardcoded option based on s3_bucket name and existing settings.
 
     Returns
     -------
@@ -251,7 +281,10 @@ def get_s3_prefix_from_product_browse_url(browse_url: str) -> str:
     """
 
     try:
-        burst_s3_prefix = browse_url.split("?prefix=")[1]
+        if not base_url:
+            burst_s3_prefix = browse_url.split("?prefix=")[1]
+        else:
+            burst_s3_prefix = browse_url.split(base_url)[1]
     except:
         raise ValueError(
             f"Could not extract valid s3 subpath from the browse link provided: {browse_url}"
@@ -261,7 +294,8 @@ def get_s3_prefix_from_product_browse_url(browse_url: str) -> str:
 
 
 def get_s3_bucket_from_product_browse_url(browse_url: str) -> str:
-    """Get the s3_bucket from the browse link - removes the s3_bucket from the url string.
+    """Get the s3_bucket from the browse link. This is hardcoded for some urls.
+      Otherwise, it attempts to remove the s3_bucket from the url string.
 
     Parameters
     ----------
@@ -280,12 +314,19 @@ def get_s3_bucket_from_product_browse_url(browse_url: str) -> str:
     -> deant-data-public-dev
 
     """
-    bucket_match = re.search(r"https:\/\/([^\.]+)\.s3", browse_url)
-    if bucket_match:
-        s3_bucket = bucket_match.group(1)
+    # hardcoded options for the bucket based on known browse urls:
+    if 'data.dev.dea.ga.gov.au' in browse_url:
+        s3_bucket = "dea-public-data-dev"
+    elif 'data.dea.ga.gov.au' in browse_url:
+        s3_bucket = "dea-public-data"
     else:
-        raise ValueError(
-            f"Could not extract s3_bucket from the browse link provided: {browse_url}"
-        )
+        # attempt to search for the bucket (appropriate for deant-data-public-dev)    
+        bucket_match = re.search(r"https:\/\/([^\.]+)\.s3", browse_url)
+        if bucket_match:
+            s3_bucket = bucket_match.group(1)
+        else:
+            raise ValueError(
+                f"Could not extract s3_bucket from the browse link provided: {browse_url}"
+            )
 
     return s3_bucket
