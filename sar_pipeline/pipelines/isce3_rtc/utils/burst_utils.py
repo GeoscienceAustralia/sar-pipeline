d0043d674843172d064c23823c249513b2f72c3b
abradley60 <alexbradley60@gmail.com>
2026-08-10 13:32:05 +0930

    maintaining iw mode capability

--- sar_pipeline/pipelines/isce3_rtc/utils/burst_utils.py
+++ sar_pipeline/pipelines/isce3_rtc/utils/burst_utils.py
@@ -159,8 +159,7 @@ def get_burst_info_for_scene_from_cdse(
     """
 
     base_url = "https://catalogue.dataspace.copernicus.eu/odata/v1/Bursts"
-    # one products per burst+pol, and can be over 100 bursts in an EW SLC
-    # set this value high to be safe.
+    # one products per burst+pol, set this value high to be safe.
     query = f"$filter=ParentProductName eq '{scene}.SAFE'&$top=500"
     url = f"{base_url}?{query}"
 
