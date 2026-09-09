03aba62da01c8cafd31a367323219a8ccbe4d243
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2026-09-09 11:40:06 +0930

    Revert "Remove NCI code"

--- docs/pipelines/README.md
+++ docs/pipelines/README.md
@@ -6,3 +6,5 @@ The repository will handle multiple pipelines and backends, some of which have s
     - [Requirements](isce3_rtc.md)
 - pyroSAR + GAMMA
     - [Requirements](pyrosar_gamma.md)
+- pyroSAR + GAMMA on the NCI
+    - [Requirements](nci_pyrosar_gamma.md)
--- sar_pipeline/preparation/downloads/scenes.py
+++ sar_pipeline/preparation/downloads/scenes.py
@@ -79,7 +79,7 @@ def query_scene_from_asf(scene: str) -> ASFSearchResults:
     scene_metadata = extract_metadata_from_s1_id(scene)
 
     # Determine the asf_search product type to search: https://github.com/asfadmin/Discovery-asf_search/blob/master/asf_search/constants/PRODUCT_TYPE.py
-    # GRD processing level query cannot currently be determined from product type alone, so list all GRD and assume it is one of these five.
+    # GRD processingly level query cannot currently be determined from product type alone, so list all GRD and assume it is one of these five.
     # The list is sufficient to distinguish metadata files from data files
     if scene_metadata.product_type == "GRD":
         processing_level_query = ["GRD_HD", "GRD_MD", "GRD_MS", "GRD_HS", "GRD_FD"]
@@ -706,7 +706,7 @@ def download_scene_from_preference_list(
     download_folder: str | Path,
     unzip: bool = True,
 ) -> tuple[Path, shapely.geometry.shape, str]:
-    f"""Download the scene from a list of data sources in order of preference
+    f"""Download the scene from a list of preferenced data sources
 
     Parameters
     ----------
@@ -797,7 +797,7 @@ def download_scene_from_preference_list_with_timeout(
 ):
     """
     Run `download_scene_from_preference_list` with a timeout. Each
-    source is allowed timeout minutes.
+    source is allowed timout minutes.
 
     Parameters
     ----------
@@ -806,7 +806,7 @@ def download_scene_from_preference_list_with_timeout(
     timeout_mins : int
         Maximum number of minutes before timing out.
     early_exit_code:
-        If a specific exit code should be raised on timeout
+        If a specific exit code should be raised on timout
     *args, **kwargs
         Passed directly to `download_scene_from_preference_list`.
 
